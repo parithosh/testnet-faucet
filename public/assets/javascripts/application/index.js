ef1ba0b6b1c635b45b2b8bebcb7a2083104f695f
Rafael Matias <rafael@skyle.net>
2021-11-30 17:57:19 +0100

    make recaptcha work

--- public/assets/javascripts/application/index.js
+++ public/assets/javascripts/application/index.js
@@ -3,19 +3,22 @@ var config = {}
 
 $.holdReady(true);
 $.getJSON("config.json", function(json) {
-	console.log(json); // show the JSON file content into console
 	config = json
 	$.holdReady(false);
 });
 
-
 $(function() {
+	grecaptcha.ready(function(){
+    grecaptcha.render("recaptcha", {
+      sitekey: config.recaptchaKey
+    });
+  });
+
 	var loader = $(".loading-container");
 	$("#logo").attr("src", config.logoUrl);
-	$("#title").html(config.title)
-	$("#footer").html(config.footer)
-	$("#requestTokens").html(config.buttonText)
-	$("#recaptcha").attr("data-sitekey", config.recaptchaKey)
+	$("#title").html(config.title);
+	$("#footer").html(config.footer);
+	$("#requestTokens").html(config.buttonText);
 
 	$( "#faucetForm" ).submit(function( e ) {
 		e.preventDefault();
@@ -27,7 +30,7 @@ $(function() {
 		  	type:"POST",
 		  	data: $this.serialize()
 		}).done(function(data) {
-			grecaptcha.reset();
+			grecaptcha.reset("recaptcha");
 			if (!data.success) {
 				loader.addClass("hidden");
 				console.log(data)
@@ -43,7 +46,7 @@ $(function() {
 			  "success"
 			);
 		}).fail(function(err) {
-			grecaptcha.reset();
+			grecaptcha.reset("recaptcha");
 			console.log(err);
 			loader.addClass("hidden");
 		});
